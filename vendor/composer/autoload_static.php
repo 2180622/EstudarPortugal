8f9049078027f371fbdf11c6f6608df075b2e4e0
Andre <machado.andref@gmail.com>
2020-03-05 10:43:05 +0000

    bootstrap4 and view editing

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -11,8 +11,8 @@ class ComposerStaticInit021477ccd5e43fe35de4329e852bba88
         '0d59ee240a4cd96ddbb4ff164fccea4d' => __DIR__ . '/..' . '/symfony/polyfill-php73/bootstrap.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '6124b4c8570aa390c21fafd04a26c69f' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
-        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         '2cffec82183ee1cea088009cef9a6fc3' => __DIR__ . '/..' . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
+        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
         '25072dd6e2470089de65ae7bf11d3109' => __DIR__ . '/..' . '/symfony/polyfill-php72/bootstrap.php',
         'f598d06aa772fa33d905e87be6398fb1' => __DIR__ . '/..' . '/symfony/polyfill-intl-idn/bootstrap.php',
@@ -32,6 +32,7 @@ class ComposerStaticInit021477ccd5e43fe35de4329e852bba88
             'yii\\debug\\' => 10,
             'yii\\composer\\' => 13,
             'yii\\bootstrap\\' => 14,
+            'yii\\bootstrap4\\' => 15,
             'yii\\' => 4,
         ),
         'p' => 
@@ -129,6 +130,10 @@ class ComposerStaticInit021477ccd5e43fe35de4329e852bba88
         'yii\\bootstrap\\' => 
         array (
             0 => __DIR__ . '/..' . '/yiisoft/yii2-bootstrap/src',
+        ),
+        'yii\\bootstrap4\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/yiisoft/yii2-bootstrap4/src',
         ),
         'yii\\' => 
         array (
--- views/universidade/index.php
+++ views/universidade/index.php
@@ -7,37 +7,50 @@ use yii\grid\GridView;
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Universidades';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="universidade-index">
+    <h4>Estatutos Académicos</h4>
+    <div class="row">
+        <div class="col-lg-4">
+            <h1>Licenciaturas</h1>
 
-    <h1><?= Html::encode($this->title) ?></h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur.</p>
 
-    <p>
-        <?= Html::a('Create Universidade', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+        </div>
+        <div class="col-lg-4">
+            <h1>Mestrados</h1>
 
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur.</p>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+        </div>
+        <div class="col-lg-4">
+            <h1>Doutoramentos</h1>
 
-            'idUniversidade',
-            'nome',
-            'morada',
-            'telefone',
-            'email:email',
-            //'NIF',
-            //'IBAN',
-            //'obsContactos:ntext',
-            //'obsCursos:ntext',
-            //'obsCandidaturas:ntext',
-            //'deleted_at',
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur.</p>
+        </div><br><br>
+    </div><br><br>
+    <div>
+        <h4>Observações</h4><br>
 
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-
-
+        <table style="width:100%">
+            <tr>
+                <th>UMA TABELA OBSERVAÇÃO</th>
+            </tr>
+            <tr>
+                <td>Uma observação kighijsdjfksdnfgnasdfkmsdlfngjasdgsdfjkgnakdsga</td>
+            </tr>
+            <tr>
+                <td>Outra observação angjakfnsfdgnalsdgasdfnakldsgnmadfnglasçdfn</td>
+            </tr>
+        </table>
+    </div>
 </div>
